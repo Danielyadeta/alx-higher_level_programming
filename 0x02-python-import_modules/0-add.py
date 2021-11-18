0ce12b779dc176d3b37db861cc7fd24466e63df8
Kidus Worku <kidusmik@gmail.com>
2021-11-18 13:43:56 +0000

    Modify 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add as add_f
 
     a = 1
     b = 2
 
-    c = add_0.add(a, b)
+    c = add_f(a, b)
 
     print("{:d} + {:d} = {:d}".format(a, b, c))
