b963cbfcc1e70d4417a2df475c24c4385eb6010a
Kidus Worku <kidusmik@gmail.com>
2021-11-18 15:18:41 +0000

    Add 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,14 @@
 if __name__ == "__main__":
     import sys
 
+str = "argument"
 len = len(sys.argv) - 1
 
+if len > 1:
+    str = str + "s"
+
 if len != 0:
-    print("{:d} arguments:".format(len))
+    print("{:d} {}:".format(len, str))
     for i in range(1, len + 1):
         print("{:d}: {}".format(i, sys.argv[i]))
 else:
