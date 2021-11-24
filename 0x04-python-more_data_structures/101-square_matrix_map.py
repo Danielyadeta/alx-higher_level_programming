a813caf4e01d3af1f60c5c1f85ddeefacdb6f35a
Kidus Worku <kidusmik@gmail.com>
2021-11-24 16:25:37 +0000

    Modify 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    return [list(map((lambda x: x * x), i)) for i in matrix]
+    return list(map((lambda rw: list(map((lambda x: x * x), rw))), matrix))
