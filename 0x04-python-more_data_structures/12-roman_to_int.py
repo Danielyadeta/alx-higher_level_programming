3f57b19cf398b0df950258eefde3b7efb163f1a5
Kidus Worku <kidusmik@gmail.com>
2021-11-24 14:30:17 +0000

    Add 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+
+
+def roman_to_int(roman_string):
+    a_dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    keys = list(a_dictionary.keys())
+    num = 0
+    for i in roman_string:
+        if i not in keys:
+            return None
+        num = num + a_dictionary.get(i)
+
+    return num
