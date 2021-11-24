c19b8bdaf51c581f8ead779ca05e64d57db92f16
Kidus Worku <kidusmik@gmail.com>
2021-11-24 14:11:04 +0000

    Add 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    max_key = None
+    if a_dictionary is not None:
+        keys = list(a_dictionary.keys())
+        max_num = a_dictionary.get(keys[0])
+        for i in keys:
+            if a_dictionary.get(i) > max_num:
+                max_num = a_dictionary.get(i)
+                max_key = i
+
+    return max_key
