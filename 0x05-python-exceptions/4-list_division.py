0049640648e64024cc792a329ebc9891f3ef425f
Kidus Worku <kidusmik@gmail.com>
2021-12-06 14:45:13 +0000

    Fix by adding finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,17 +5,19 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            result.append(my_list_1[i] / my_list_2[i])
+            c = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
-            result.append(0)
+            c = 0
             continue
         except (TypeError, ValueError):
             print("wrong type")
-            result.append(0)
+            c = 0
             continue
         except IndexError:
             print("out of range")
-            result.append(0)
+            c = 0
             continue
+        finally:
+            result.append(c)
     return result
