fcc35b73e1eae4901d8f9ec96dc2136103496a41
Kidus Worku <kidusmik@gmail.com>
2021-12-06 14:41:21 +0000

    Fix error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,5 +17,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
-            break
+            continue
     return result
