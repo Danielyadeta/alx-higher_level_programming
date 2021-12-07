f0b5354429e804a6f41018192e0b09db86ab4ef3
Kidus Worku <kidusmik@gmail.com>
2021-12-07 14:45:34 +0000

    Add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Definition of Square class"""
+
+
+class Square:
+    """Defines a class square.
+
+    Attributes:
+        __size: Size of the square
+
+    Args:
+        size: Size of the square
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
