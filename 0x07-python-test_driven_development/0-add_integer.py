5b9b6cb802c291bc1c31ff290b693e50712f2a05
Kidus Worku <kidusmik@gmail.com>
2021-12-10 08:21:08 +0000

    Fix style

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,13 +7,14 @@ The addition module supplies one function, add_integer(). For example,
 add_integer(1, 2) returns 3.
 """
 
+
 def add_integer(a, b=98):
     """ Returns the addition of a and b, an exact integer."""
     if type(a) is not int and\
-        type(a) is not float:
+            type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and\
-        type(b) is not float:
+            type(b) is not float:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
