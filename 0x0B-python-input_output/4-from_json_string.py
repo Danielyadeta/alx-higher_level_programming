0f424b2f9b7dfeab598ff93251b78cb934a6cc74
Kidus Worku <kidusmik@gmail.com>
2021-12-21 16:23:10 +0000

    Add 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""defining from_json_string function"""
-
-
+"""
+This is the "0-read_file" module.
+The 0-read_file module supplies one function, read_file().
+"""
 import json
 
 
 def from_json_string(my_str):
-    """return json string"""
+    """returns an object (Python data structure) represented by a JSON string"""
     return json.loads(my_str)
