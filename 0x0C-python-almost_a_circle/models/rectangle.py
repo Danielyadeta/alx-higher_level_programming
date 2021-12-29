63a02438b0c61a67d11675c3578f1d47d4c3e1a1
Kidus Worku <kidusmik@gmail.com>
2021-12-29 15:35:08 +0000

    Update rectangle.py by improving display function, Add tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -133,7 +133,11 @@ class Rectangle(Base):
 
     def display(self):
         """Prints a diagram of the rectangle using the # symbol."""
+        for k in range(self.__y):
+            print()
         for i in range(self.__height):
+            for m in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
