51a17d7b59ac6e158f4774bd8c41cfa6b19b1f9f
Kidus Worku <kidusmik@gmail.com>
2021-12-29 09:29:59 +0000

    Update rectangle.py with printing and Add tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,3 +113,9 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of the rectangle."""
         return self.__width * self.__height
+
+    def display(self):
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
